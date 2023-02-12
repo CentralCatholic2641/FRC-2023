e68345de9b619d10194d28f17a951219fe769690
XxGladiatorxX1 <117952565+XxGladiatorxX1@users.noreply.github.com>
2023-02-12 15:01:18 -0500

    aDDeD shOulDeR SuBSystEM

---
+++ src/main/java/frc/team2641/frc2023/subsystems/Shoulder.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.team2641.frc2023.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team2641.frc2023.Constants;
+
+public class Shoulder extends SubsystemBase {
+  /** Creates a new Shoulder. */
+  private static Shoulder instance = null;
+
+  public static Shoulder getInstance() {
+    if (instance == null)
+      instance = new Shoulder();
+    return instance;
+  }
+
+  private WPI_TalonFX leftShoulder = new WPI_TalonFX(Constants.CAN.leftShoulder);
+  private WPI_TalonFX rightShoulder = new WPI_TalonFX(Constants.CAN.rightShoulder);
+
+  public Shoulder() {}
+
+public void set(double value){
+leftShoulder.set(value);
+rightShoulder.set(-value);
+}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
